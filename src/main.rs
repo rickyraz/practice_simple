d26f4a7c9f31cd33587a70c02930c40d232b14c2
rickyrazhari <rickyraihan83@gmail.com>
2022-11-21 16:30:04 +0700

    simplify

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,49 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use std::collections::HashMap;
 use std::io;
 
-fn main() {
-    // temp_calc();
-    // nth_fibonacci_number();
-    // area_of_rectangle();
 
-    let mut users = ["John", "Jane", "Jack", "Jill"];
-    println!("Before: {}", users[0]);
 
-    // mutate array element 0
-    users[0] = "John Doe";
-    println!("After: {}", users[0]);
+fn mode_median_average() {
+    fn average(numbers: &[i32]) -> f32 {
+        numbers.iter().sum::<i32>() as f32 / numbers.len() as f32
+    }
+    
+    fn median(numbers: &mut [i32]) -> f64 {
+        // numbers.sort();
+        // let mid = numbers.len() / 2;
+        // numbers[mid]
+    
+        if (numbers.len() % 2) == 0 {
+            let ind_left = numbers.len() / 2 - 1;
+            let ind_right = numbers.len() / 2;
+            (numbers[ind_left] + numbers[ind_right]) as f64 / 2.0
+        } else {
+            numbers[(numbers.len() / 2)] as f64
+        }
+    }
+    
+    fn mode(numbers: &[i32]) -> i32 {
+        let mut occurrences = HashMap::new();
+    
+        for &value in numbers {
+            *occurrences.entry(value).or_insert(0) += 1;
+        }
+    
+        occurrences
+            .into_iter()
+            .max_by_key(|&(_, count)| count)
+            .map(|(val, _)| val)
+            .expect("Cannot compute the mode of zero numbers")
+    }
+
+    let mut numbers = [42, 1, 36, 34, 76, 378, 43, 1, 43, 54, 2, 3, 43, 78];
+
+    println!("AVERAGE: {}", average(&numbers));
+    println!("MEDIAN: {}", median(&mut numbers));
+    println!("MODE: {}", mode(&numbers));
 }
 
 fn temp_calc() {
@@ -72,6 +102,40 @@ fn nth_fibonacci_number() {
 }
 
 fn area_of_rectangle() {
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    // reading (&self), mutating (&mut self), or consuming (self).
+    impl Rectangle {
+        fn area(&self) -> u32 {
+            self.width * self.height
+        }
+        fn width(&self) -> bool {
+            self.width > 0
+        }
+        // Methods with More Parameters
+        fn can_hold(&self, other: &Rectangle) -> bool {
+            self.width > other.width && self.height > other.height
+        }
+    }
+
+    // -- The area function is supposed to calculate the area of one rectangle, but the function we wrote has two parameters, and it’s not clear anywhere in our program that the parameters are related. It would be more readable and more manageable to group width and height together.
+
+    // fn area(width: u32, height: u32) -> u32 {
+    //     width * height
+    // }
+
+    // fn area2(dimensions: (u32, u32)) -> u32 {
+    //     dimensions.0 * dimensions.1
+    // }
+
+    fn area3(rectangle: &Rectangle) -> u32 {
+        rectangle.width * rectangle.height
+    }
+
     // let width = 30;
     // let height = 50;
     // let rect1 = (24, 90);
@@ -110,6 +174,7 @@ fn area_of_rectangle() {
     );
 
     println!(
+        // cause addition to providing method syntax and not having to repeat the type of self in every method’s signature.
         "The area of the rectangle is {} square pixels - using impl area.",
         rect2.area()
     );
@@ -120,40 +185,15 @@ fn area_of_rectangle() {
 
     println!("Can rect1 hold rect2? {}", rect2.can_hold(&rect3));
     println!("Can rect1 hold rect3? {}", rect2.can_hold(&rect4));
-
     println!("rect2 is {:?}", rect2);
+}
 
-    #[derive(Debug)]
-    struct Rectangle {
-        width: u32,
-        height: u32,
-    }
+// -----------------
 
-    // reading (&self), mutating (&mut self), or consuming (self).
-    impl Rectangle {
-        fn area(&self) -> u32 {
-            self.width * self.height
-        }
-        fn width(&self) -> bool {
-            self.width > 0
-        }
-        // Methods with More Parameters
-        fn can_hold(&self, other: &Rectangle) -> bool {
-            self.width > other.width && self.height > other.height
-        }
-    }
-
-    // -- The area function is supposed to calculate the area of one rectangle, but the function we wrote has two parameters, and it’s not clear anywhere in our program that the parameters are related. It would be more readable and more manageable to group width and height together.
-
-    // fn area(width: u32, height: u32) -> u32 {
-    //     width * height
-    // }
-
-    // fn area2(dimensions: (u32, u32)) -> u32 {
-    //     dimensions.0 * dimensions.1
-    // }
-
-    fn area3(rectangle: &Rectangle) -> u32 {
-        rectangle.width * rectangle.height
-    }
+fn main() {
+    // temp_calc();
+    // nth_fibonacci_number();
+    // area_of_rectangle();
+    // array();
+    mode_median_average();
 }
